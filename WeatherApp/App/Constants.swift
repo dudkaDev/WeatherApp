02ceaa4d2e08f8c9d7efb76b0865da74c9d23c43
Andrey Abakumov <andrey9864rus@mail.ru>
2022-10-23 20:07:13 +0400

    Add labels, button to view controller. Started preparing the model for JSON reception.

---
+++ WeatherApp/App/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  WeatherApp
+//
+//  Created by Андрей Абакумов on 23.10.2022.
+//
+
+import Foundation
+
+let apiKey = "af054d2df7fdc866863ec1bedc09035a"
---
+++ WeatherApp/View Controllers/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Андрей Абакумов on 23.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var weatherIconImageView: UIImageView!
+    @IBOutlet var cityLabel: UILabel!
+    @IBOutlet var temperatureLabel: UILabel!
+    @IBOutlet var feelsLikeTemperatureLabel: UILabel!
+    
+    let networkWeatherManager = NetworkWeatherManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        networkWeatherManager.fetchCurrentWeather(forCity: "London")
+    }
+
+    
+    @IBAction func searchButtonPressed(_ sender: UIButton) {
+        searchAlertController(withTitle: "Enter city name", message: nil, style: .alert)
+    }
+    
+    private func searchAlertController(withTitle title: String, message: String?, style: UIAlertController.Style) {
+        
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: style
+        )
+        
+        let search = UIAlertAction(title: "Search", style: .default) { _ in
+            let textField = alert.textFields?.first
+            guard let cityName = textField?.text else { return }
+            if cityName != "" {
+                print("search info for the \(cityName)")
+            }
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        alert.addTextField()
+        alert.addAction(search)
+        alert.addAction(cancel)
+        present(alert, animated: true)
+    }
+}
+
